495ffc6d137c4f56f7229d03ac9b44f62e5a2473
ArtemWo <you@example.com>
2022-10-09 17:40:25 +0200

    DZ

---
+++ JavaTASK90922.java
@@ -0,0 +1,20 @@
+/*
+package TasksJava;
+
+import java.util.Scanner;
+
+public class JavaTASK90922 {
+    public static void main(String[] args) {
+
+    */
+/*     String s = "The area of a rectangle with sides 21 and 17";
+         int a = 21, b=17;
+        System.out.println(s + "S= " +(a * b));*//*
+
+
+
+    }
+
+
+}
+*/
---
+++ MainHWTasks26_09_22.java
@@ -0,0 +1,78 @@
+/*
+package TasksJava;
+
+public class MainHWTasks26_09_22 {
+    public static void main(String[] args) {
+    */
+/*First level: Напишите метод, который принимает три числа и определяет в каком порядке они введены:
+    печатает increasing если числа возрастают (1,2.3),  decreasing если числа введены в убывающем порядке
+    (3,2,1) и "neither increasing nor decreasing order" во всех остальных случаях.*//*
+
+
+        System.out.println(num(1, 9, 8));
+    }
+
+    public static int num(int a, int b, int c) {
+
+        return a;
+    }
+
+}
+
+        */
+/*if (b > c) {
+            int x = b;
+            b = c;
+            c = x;
+        }
+        if (a > c) {
+            int x = a;
+            a = c;
+            c = x;
+        }
+        if (a > b) {
+            int x = a;
+            a = b;
+            b = x;
+        }
+        return a;{
+
+        }
+        System.out.println("Increasing: " + a + " " + b + " " + c);
+
+        if (b < c) {
+            int y = b;
+            b = c;
+            c = y;
+
+            if (a < c) {
+                int y = a;
+                a = c;
+                c = y;
+            }
+            if (a < b) {
+                int y = a;
+                a = b;
+                b = y;
+                System.out.println("Decreasing: " + a + " " + b + " " + c);
+            }
+            return a;
+        }*//*
+
+                     */
+/*  Second level: Вася рано встает на работу и поэтому рано ложится спать. Сосед Васи любит слушать громкую музыку.
+    У Васи проблемы если сосед слушает музыку и еще нет семи утра или уже позже 20 часов. Напишите метод, возвращающий
+    true если у Васи проблемы.
+    boolean vasiaInTrouble (boolean singing, int hour)
+    где hour - время в часах в диапазоне от 0 до 23
+    например:
+    vasiaInTrouble (true, 6) -> true
+    vasiaInTrouble (true, 8) -> false
+    vasiaInTrouble (false, 6) -> false*//*
+
+
+
+
+
+
+*/
