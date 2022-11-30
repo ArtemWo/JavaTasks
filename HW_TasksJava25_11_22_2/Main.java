821191be2aa4f5955cb6a05a9d5834eeb3afa98f
ArtemWo <111278541+ArtemWo@users.noreply.github.com>
2022-11-30 22:13:11 +0100

    dz 25/11/22

--- HW_TasksJava25_11_22_2/Main.java
+++ HW_TasksJava25_11_22_2/Main.java
@@ -10,6 +10,7 @@ public class Main {
 
     /*todo:Есть лист  целых чисел. Написать метод, который вернет список без элементов, больше числа n.
        public List withoutElementsMoreThanN(List ints, int n)  */
+        
         List<Integer> listNum = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
         System.out.println(withoutElementsMoreThanN(listNum,2));
     }
@@ -33,17 +34,4 @@ public class Main {
 
 
 
-            /*List<Integer> intArr = Arrays.asList(1, 5, 14, 99, 102, 100, 25, 78, 12, 3, 4, 6, 126, 259);
-            System.out.println(withoutElementsMoreThanN(intArr, 126));
-        }
-
-        public static List<Integer> withoutElementsMoreThanN (List < Integer > arr,int n){
-            List<Integer> newArr = new ArrayList<>();
-            for (int i = 0; i < arr.size(); i++) {
-                if (arr.get(i) <= n) {
-                    newArr.add(arr.get(i));
-                }
-            }
-            return newArr;
-        }*/
-
+           
